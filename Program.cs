d1692dda99b894a645aedea32df385a04b8bc262
ahlmatj <ahlmatj@dunwoody.edu>
2020-02-28 13:04:42 -0600

    added in the framework for the Working and ToString methods (as well as comments for what they do) and removed an accidental instance of the TotalEstimate property located in the Estimate method

--- Program.cs
+++ Program.cs
@@ -20,14 +20,25 @@ namespace assignment_one
         public float TotalEstimate {get; set;}
 
         public Estimate(string DogOwner, string DogName, int DogWeight,//This is the Estimate method
-            int StayDuration, string ServiceAddon, float TotalEstimate)
+            int StayDuration, string ServiceAddon)
             {
                 this.DogOwner = DogOwner; //The "this" keyword refers to the thing stored in current method instance
                 this.DogName = DogName;//For example, this.DogOwner = DogOwner is telling the program to assign the data passed through the method to the actual variable known as DogOwner
                 this.DogWeight = DogWeight;//TLDR; this.DogOwner = DogOwner is assigning the user input into the DogOwner variable
                 this.StayDuration = StayDuration;
                 this.ServiceAddon = ServiceAddon;
-                this.TotalEstimate = TotalEstimate;
             }
+
+        public working()//This method processes the ServiceAddon code and calculates the TotalEstimate
+        {
+
+        }
+
+        public ToString()//This method outputs the data to the user
+        {
+
+        }
+
     }
+
 }
