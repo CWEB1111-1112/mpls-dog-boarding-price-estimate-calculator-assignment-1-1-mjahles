61ae7fa95da74cb5fc27c730821c42b8a08a257c
ahlmatj <ahlmatj@dunwoody.edu>
2020-02-28 11:49:10 -0600

    created properties for the Estimate class and added in the Estimate method

--- Program.cs
+++ Program.cs
@@ -9,4 +9,24 @@ namespace assignment_one
             Console.WriteLine("Hello World!");
         }
     }
+    class Estimate
+    {
+        public string DogOwner {get; set;}
+        public string DogName {get; set;}
+        public string DogWeight {get; set;}
+        public string StayDuration {get; set;}
+        public string ServiceAddon {get; set;}
+        public string TotalEstimate {get; set;}
+
+        public Estimate(string DogOwner, string DogName, int DogWeight,
+            int StayDuration, string ServiceAddon, string TotalEstimate)
+            {
+                this.DogOwner = DogOwner;
+                this.DogName = DogName;
+                this.DogWeight = DogWeight;
+                this.StayDuration = StayDuration;
+                this.ServiceAddon = ServiceAddon;
+                this.TotalEstimate = TotalEstimate;
+            }
+    }
 }
